6bb690f4008530e22ccf39b212882edd1d4b3805
Rafael <rreynoud@gmail.com>
2019-09-02 21:21:03 -0300

    Adicionado rotas

--- vue-rotas/src/App.vue
+++ vue-rotas/src/App.vue
@@ -8,6 +8,8 @@
 
     <div class="container">
 
+      <router-link to="/" class="btn btn-info mb-2 mr-2"  > Inicio </router-link>
+      <router-link to="/contatos" class="btn btn-info mb-2 mr-2 "  >Contatos</router-link>
       <router-view></router-view>
 
     </div>
--- vue-rotas/src/main.js
+++ vue-rotas/src/main.js
@@ -9,6 +9,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode:'history',
   routes: [
     {path: '/contatos', component: Contatos}, //meus-contatos.com/contato
     {path: '/', component: Home} //meus-contatos
